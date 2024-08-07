72a3d3bd0fd5d4d0882c64a7336e8364673c3abe
Azeez Adebayo <debaycisse@gmail.com>
2024-08-07 22:08:52 +0100

    implement task 0. Minimum Operations
    function finds and returns a fewest number of operations
    (such as Copy All and Past operations) to be carried out
    in order to have H in n times

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""This module houses the definition of functions that are used to
+calculates the fewest number of operations needed to take place
+for the H to copied in a given number times"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number operation, needed to result in having
+    letter H in n times. The just two operations are Copy All and Paste
+
+    Args:
+        n - the number of times to have letter 'H' endup in
+
+    Returns:
+        the number of fewest operations needed to place
+    """
+    op_count = [0]
+
+    def find_div(num):
+        """
+        Finds the highest divisor for the given number
+
+        Args:
+            num - the number whose highest divisor is to be obtained
+
+        Returns:
+            the (highest) number among the possible divisors
+        """
+        i = 2
+        val = num
+        data = [0]
+        while i <= val:
+            if (val % i == 0):
+                data[0] = i
+                val //= i
+            i += 1
+        return data[-1]
+
+    def copy_all_op():
+        """Simulates Copy All operation"""
+        op_count[0] += 1
+
+    def paste_op():
+        """Simulates Paste operation"""
+        op_count[0] += 1
+
+    if ((n <= 0) or (not isinstance(n, int))):
+        return 0
+    divisor_value = find_div(n)
+    division_value = n // divisor_value
+    copy_all_op()
+    for _ in range(divisor_value - 1):
+        paste_op()
+    if (op_count[0] < n):
+        copy_all_op()
+    for _ in range(division_value - 1):
+        paste_op()
+    return op_count[-1]
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# Minimum Operations
+
+This dicrectory collects a set of tasks that are related to the concept of finding a fewest number of operation to carryout a given task
