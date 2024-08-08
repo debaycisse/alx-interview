c1862f7685450d4a84c5626c1004ea82a7e9b1b4
Azeez Adebayo <debaycisse@gmail.com>
2024-08-08 11:02:24 +0100

    apply annotation to all functions

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,7 +4,7 @@ calculates the fewest number of operations needed to take place
 for the H to copied in a given number times"""
 
 
-def minOperations(n):
+def minOperations(n: int) -> int:
     """
     Calculates the fewest number operation, needed to result in having
     letter H in n times. The just two operations are Copy All and Paste
@@ -16,8 +16,9 @@ def minOperations(n):
         the number of fewest operations needed to place
     """
     op_count = [0]
+    content = ['H']
 
-    def find_div(num):
+    def find_div(num: int) -> int:
         """
         Finds the highest divisor for the given number
 
@@ -37,13 +38,14 @@ def minOperations(n):
             i += 1
         return data[-1]
 
-    def copy_all_op():
+    def copy_all_op() -> None:
         """Simulates Copy All operation"""
         op_count[0] += 1
 
-    def paste_op():
+    def paste_op() -> None:
         """Simulates Paste operation"""
         op_count[0] += 1
+        content[0] += 'H'
 
     if ((not isinstance(n, int)) or (n <= 0) or (n == 1)):
         return 0
@@ -52,8 +54,8 @@ def minOperations(n):
     copy_all_op()
     for _ in range(divisor_value - 1):
         paste_op()
-    if (op_count[0] < n):
+    if (len(content[0]) != n):
         copy_all_op()
-    for _ in range(division_value - 1):
-        paste_op()
+        for _ in range(division_value - 1):
+            paste_op()
     return op_count[-1]
