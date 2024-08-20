55ec7fe535b194328acf0af95d7dbcf2d868e08c
Azeez Adebayo <debaycisse@gmail.com>
2024-08-20 09:48:50 +0100

    implement task 0
    script checks if a given number is a valid UTF8 code point
    for the charactersets at the first 1 byte of the whole 4
    bytes of it (UTF8)

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,70 @@
 #!/usr/bin/python3
 """
-#TODO:
+This module houses the definiton of a function, named validUTF8
 """
+from typing import List, Union
 
 
 def convert_to_base_two(num: int) -> str:
     """
-    #TODO:
+    Converts a given number to its binary form
+
+    Args:
+        num - the number whose binary is to be obtained
+
+    Returns:
+        the binary representation of the given number
     """
-    a = num
-    b = []
-    c = '0'
+    a: int = num
+    b: List[int] = []
+    c: str = '0'
     while a > 1:
         b.insert(0, a % 2)
         a //= 2
-    
     for num in b:
-        c += num
-
+        c += '{0}'.format(num)
     return c
+
+
+def is_int_list(data: List[int]) -> bool:
+    """
+    Confirms that a given data variable is a list of integers
+
+    Args:
+        data - the variable to be validated
+
+    Returns:
+        True, if a given variable is a list of integers, otherwise false
+    """
+    for num in data:
+        if not isinstance(num, int):
+            return False
+    return True
+
 
 def validUTF8(data: Union[List[int], int]) -> bool:
     """
-    #TODO:
+    Confirms that a given data (integer) is a valid UTF8
+    character set, found in its 1 byte code point
+
+    Args:
+        data - the data, which may be a number or
+        list of number to be validated
+
+    Returns:
+        True, if it is a valid UTF8 character set, otherwise false
     """
+    if (not isinstance(data, int)) and (not is_int_list(data)):
+        raise ValueError('data must be integer or list of integer')
+
     if isinstance(data, list):
-        #TODO:  Do the below in an iteration
-        # call convert_to_base_two function to obtain the binary of a number
-        # check if the length is greater than 8 bits
-            # YES:  return FALSE
-            # NO:  return TRUE
-            
-    else:
-        # call convert_to_base_two function to obtain the binary of a number
-        # check if the length is greater than 8 bits
-            # YES:  return FALSE
-            # NO:  return TRUE
+        for num in data:
+            res: str = convert_to_base_two(num)
+            if len(res) > 8:
+                return False
+        return True
+
+    res = convert_to_base_two(data)
+    if len(res) > 8:
+        return False
+    return True
