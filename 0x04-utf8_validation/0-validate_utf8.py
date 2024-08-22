952f070259ec4c4c02b99f526c779a028b8b6224
Azeez Adebayo <debaycisse@gmail.com>
2024-08-22 13:42:53 +0100

    ensure pep8 compliance

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,10 +2,9 @@
 """
 This module houses the definiton of a function, named validUTF8
 """
-from typing import List, Union
 
 
-def validUTF8(data: Union[List[int], int]) -> bool:
+def validUTF8(data):
     """
     Confirms that a given data (integer) is a valid UTF8
     character set, found in its 1 byte code point
@@ -17,16 +16,15 @@ def validUTF8(data: Union[List[int], int]) -> bool:
     Returns:
         True, if it is a valid UTF8 character set, otherwise false
     """
-    if (not isinstance(data, list)) and (not isinstance(data, int)):
+    if(not isinstance(data, list)) and (not isinstance(data, int)):
         return
-    is_valid: bool
-    if isinstance(data, list):
+    if(isinstance(data, list)):
         for num in data:
             is_valid = bytes([num]).isascii()
             if not is_valid:
                 return False
         return True
-    if isinstance(data, int):
+    if(isinstance(data, int)):
         is_valid = bytes([data]).isascii()
         if not is_valid:
             return False
