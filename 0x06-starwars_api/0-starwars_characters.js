82242c1e70cd2e60ab92bcacce910596b675c030
Azeez Adebayo <debaycisse@gmail.com>
2024-09-08 21:55:43 +0100

    implement task 0
    0. Star Wars Characters
    fetches star wars characters' names

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,102 +2,48 @@
 /* This script fetches a list of characters' names for a given movie ID */
 const request = require('request');
 
-try {
-  const movieID = parseInt(process.argv[2]);
-} catch (error) {
-  console.error(error);
-  return;
+const movieID = parseInt(process.argv[2]);
+
+if (isNaN(movieID)) {
+  console.log(`Usage: ${process.argv[1]} <movie's id>`);
+  process.exit(1);
+}
+
+async function fetchCharacterObject (characterUrl) {
+  /* Fetches a character's object whose url is given */
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const data = JSON.parse(body);
+        resolve(data);
+      } else {
+        reject(new Error('Could not fetch - ', characterUrl));
+      }
+    });
+  });
+}
+
+async function fetchCharacterNames (characterUrls) {
+  /* Fetches and prints out all names of characters whose urls are given */
+  for (const url of characterUrls) {
+    try {
+      const characterObject = await fetchCharacterObject(url);
+      const characterName = characterObject.name;
+      console.log(characterName);
+    } catch (error) {
+      console.error('Error occured while fetching user data.', error.message);
+    }
+  }
 }
 
 const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-request(url, 
+request(url,
   (error, response, body) => {
     if (!error && response.statusCode === 200) {
-       const characterUrls = JSON.parse(body).characters;
-       for (const characterUrl of characterUrls) {
-         const characterData = new Promise((resolve, reject) => {
-           
-	 });
-         const characterName = await characterData.name
-       }
+      const characterUrls = JSON.parse(body).characters;
+      fetchCharacterNames(characterUrls);
+    } else {
+      console.error(`Movie ID ${movieID} not found`);
     }
   }
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function fetchData(data, fromUrl){
-  /*Fetches a given data frm a given url*/
-  
-}
-
-
-async function fetchCharacters(movieID) {
-  /**
-   * Fetches a list of names of the characters for the given movie's id
-   */
-  try {
-    request(`https://swapi-api.alx-tools.com/api/films/${movieID}`, async (error, res, body) => {
-      if (!error && res.statusCode === 200) {
-        const characterUrls = JSON.parse(body).characters;
-        for (const characterUrl of characterUrls) {
-          try {
-            await request(characterUrl, (err, resp, resBody) => {
-              if (!error && resp.statusCode === 200) {
-                const characterName = JSON.parse(resBody).name;
-                console.log(characterName)
-	      }
-	    });
-          } catch (error) {
-            console.error(error.message);
-          }
-        }
-      }
-    });
-  } catch (error) {
-    console.error(error.message)
-  }
-}
-
-fetchCharacters(process.argv[2]);
